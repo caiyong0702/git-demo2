2ee350109e92c3019131268d2fa0748a03b317a8
caiyong0702 <caiyong420@163.com>
2021-06-02 09:18:32 +0800

    second commit

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String hello(){
+        System.out.println("hello world");
         return "hello world";
     }
+
 }
