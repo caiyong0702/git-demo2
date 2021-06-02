eb28a4bad8849dd1be7afed9816c3941459223ee
caiyong0702 <caiyong420@163.com>
2021-06-02 09:19:22 +0800

    third commit

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -9,6 +9,7 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello(){
         System.out.println("hello world");
+        System.out.println("hello world !");
         return "hello world";
     }
 
